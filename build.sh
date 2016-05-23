0ece346ab12360b276c11a7d3917cbd7c946ab45
John T Skarbek <jtslear@gmail.com>
2016-05-22 20:29:48 -0400

    correct tagging mechanism

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -a -installsuffix cgo -o main
 upx -o main.up main
 
 docker build -t jtslear/testapp:latest .
-docker jtslear/testapp:latest jtslear/testapp:$VERSION
+docker tag jtslear/testapp:latest jtslear/testapp:$VERSION
 
 docker push jtslear/testapp:latest
 docker push jtslear/testapp:$VERSION
