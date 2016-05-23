addafbfee2dcaba690c7132bc3373577bfaad504
John T Skarbek <jtslear@gmail.com>
2016-05-22 22:59:17 -0400

    simplify the code

--- helloweb.go
+++ helloweb.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	read, err := ioutil.ReadFile("VERSION")
-	if err != nil {
-		fmt.Println("Error:", err)
-	}
-	io.WriteString(w, string(read[:]))
+var VERSION int = 11
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, strconv.Itoa(VERSION))
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":8000", nil)
 }
